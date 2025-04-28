526dab3df9c3a03b219bd314508e3c9c726f5eca
amirhosseinnorouzigorji <amirhosseinnorouzigorji@gmail.com>
2025-04-28 10:37:15 +0330

    Add project files.

---
+++ DNSController/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DNSController
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.lbl_connection_status = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label1.Font = new System.Drawing.Font("Sarbaz", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(1, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(332, 98);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "شکن دسکتاپ";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnConnect.BackColor = System.Drawing.Color.Gainsboro;
+			this.btnConnect.FlatAppearance.BorderSize = 0;
+			this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnConnect.Font = new System.Drawing.Font("Sarbaz", 18F);
+			this.btnConnect.Location = new System.Drawing.Point(67, 378);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(186, 65);
+			this.btnConnect.TabIndex = 1;
+			this.btnConnect.Text = "اتصال";
+			this.btnConnect.UseVisualStyleBackColor = false;
+			this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+			// 
+			// lbl_connection_status
+			// 
+			this.lbl_connection_status.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.lbl_connection_status.Font = new System.Drawing.Font("Vazir", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_connection_status.ForeColor = System.Drawing.Color.Red;
+			this.lbl_connection_status.Location = new System.Drawing.Point(0, 468);
+			this.lbl_connection_status.Name = "lbl_connection_status";
+			this.lbl_connection_status.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.lbl_connection_status.Size = new System.Drawing.Size(332, 35);
+			this.lbl_connection_status.TabIndex = 2;
+			this.lbl_connection_status.Text = "قطع است.";
+			this.lbl_connection_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pictureBox1.Image = global::DNSController.Properties.Resources.shecan;
+			this.pictureBox1.Location = new System.Drawing.Point(1, 98);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(332, 267);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.btnConnect);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(332, 503);
+			this.panel1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(332, 503);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.lbl_connection_status);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximumSize = new System.Drawing.Size(350, 550);
+			this.MinimumSize = new System.Drawing.Size(350, 550);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "شکن";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnConnect;
+		private System.Windows.Forms.Label lbl_connection_status;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ DNSController/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DNSController
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private bool connect = false;
+		private void btnConnect_Click(object sender, EventArgs e)
+		{
+			panel1.Enabled = false;
+			if (connect)
+			{
+				// کد قطع کردن اتصال
+				Disconnect();
+				btnConnect.Text = "اتصال";
+				lbl_connection_status.Text = "قطع است.";
+				lbl_connection_status.ForeColor = Color.Red;
+				connect = false;
+			}
+			else
+			{
+				// کد وصل کردن اتصال
+				Connect();
+				btnConnect.Text = "قطع";
+				lbl_connection_status.Text = "وصل است.";
+				lbl_connection_status.ForeColor = Color.MediumSeaGreen;
+				connect = true;
+			}
+			panel1.Enabled = true;
+		}
+		private void Connect()
+		{
+			string networkInterfaceName = GetNetworkInterfaceName("Wi-Fi");
+			if (string.IsNullOrEmpty(networkInterfaceName))
+			{
+				MessageBox.Show("شبکه Wi-Fi پیدا نشد!", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				SetDNS(networkInterfaceName, "185.51.200.2", "178.22.122.100");
+				MessageBox.Show("DNS با موفقیت تنظیم شد!", "موفق", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("خطا در تنظیم DNS: " + ex.Message, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		private void Disconnect()
+		{
+			string networkInterfaceName = GetNetworkInterfaceName("Wi-Fi");
+			if (string.IsNullOrEmpty(networkInterfaceName))
+			{
+				MessageBox.Show("شبکه Wi-Fi پیدا نشد!", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				ResetDNS(networkInterfaceName);
+				MessageBox.Show("DNS به حالت پیش‌فرض بازگردانده شد!", "موفق", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("خطا در بازگرداندن DNS: " + ex.Message, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		// متد برای یافتن نام کارت شبکه
+		private string GetNetworkInterfaceName(string description)
+		{
+			var Networks = NetworkInterface.GetAllNetworkInterfaces();
+			foreach (NetworkInterface ni in Networks)
+			{
+				if (ni.OperationalStatus == OperationalStatus.Up && (ni.Description.Contains(description) || ni.Name.Contains(description)))
+				{
+					return ni.Id;
+				}
+			}
+			return null;
+		}
+		// متد برای تنظیم DNS
+		private void SetDNS(string networkInterfaceName, string primaryDns, string secondaryDns)
+		{
+			ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+			ManagementObjectCollection moc = mc.GetInstances();
+
+			foreach (ManagementObject mo in moc)
+			{
+				if ((bool)mo["IPEnabled"] && mo["SettingID"].ToString() == networkInterfaceName)
+				{
+					ManagementBaseObject newDNS = mo.GetMethodParameters("SetDNSServerSearchOrder");
+					newDNS["DNSServerSearchOrder"] = new string[] { primaryDns, secondaryDns };
+					mo.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+				}
+			}
+		}
+
+		// متد برای بازگرداندن DNS به حالت پیش‌فرض
+		private void ResetDNS(string networkInterfaceName)
+		{
+			ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+			ManagementObjectCollection moc = mc.GetInstances();
+
+			foreach (ManagementObject mo in moc)
+			{
+				if ((bool)mo["IPEnabled"] && mo["SettingID"].ToString() == networkInterfaceName)
+				{
+					ManagementBaseObject resetDNS = mo.GetMethodParameters("SetDNSServerSearchOrder");
+					resetDNS["DNSServerSearchOrder"] = null; // بازگرداندن به DHCP
+					mo.InvokeMethod("SetDNSServerSearchOrder", resetDNS, null);
+				}
+			}
+		}
+	}
+}
